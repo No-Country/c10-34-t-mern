7af403938b75a934ac3abb36b31c6196311fcd20
Rikardo Akosta <richy.andres.acosta@gmail.com>
2023-04-03 01:26:31 +0200

    se crean los modelos con su respectiva relacion y README para hacer explicacion de estos modelos

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
-import app from "./src/app.js"
-import "dotenv/config"
+import app from "./src/app.js";
+import "dotenv/config";
 import connectDB from "./src/database/database.js";
-const PORT=process.env.PORT||5000
+import User from "./src/models/Users.js";
+import Appointment from "./src/models/Appointment.js";
+import Service from "./src/models/Service.js";
+import Product from "./src/models/Product.js";
 
-connectDB()
-app.listen(PORT,()=>(console.log(`***SERVER IS RUNING ON PORT ${PORT}***`)))
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+app.listen(PORT, () => console.log(`***SERVER IS RUNING ON PORT ${PORT}***`));
--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -1,41 +1,43 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name:{
-    type:String
-  },
-  lastName:{
-    type:String
-  },
-  email:{
-    type:String,
-    unique:true
-  },
-  dateOfBirth:{
-    type:String
-  },
-  password:{
-    type:String,
-  },
-  homeAddress:{
-    type:String
-  },
-  age:{
-    type:Number
-  },
-  role:{
-    type:["user","admin"],
-    default:"user"
-  },
-  status:{
-    type:String,
-    default:"active"
-  }
-},
+const UserSchema = new mongoose.Schema(
   {
-    timestamps:true,
-    versionKey:false
+    name: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    dateOfBirth: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    homeAddress: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    role: {
+      type: ["user", "admin"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      default: "active",
+    },
+    isBarber: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
   }
-)
+);
 
-export default mongoose.model('Users',UserSchema)
+export default mongoose.model("Users", UserSchema);
