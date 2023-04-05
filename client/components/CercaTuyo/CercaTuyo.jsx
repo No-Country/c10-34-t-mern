fdfe2a789c4e45aaf2faca9623e4ec004f189ecc
nasanchez7 <nadirblanco02@gmail.com>
2023-04-05 16:53:10 -0300

    avanzando en pagina de inicio

--- client/components/CercaTuyo/CercaTuyo.jsx
+++ client/components/CercaTuyo/CercaTuyo.jsx
@@ -1,10 +1,13 @@
-import { ScrollView, View, Text, StyleSheet, TextInput, FlatList } from "react-native"
+import { View, Text, StyleSheet, TextInput, FlatList, TouchableOpacity } from "react-native"
 import { AntDesign } from '@expo/vector-icons';
 import CardXl from "../CardXl/CardXl";
 import CardS from "../CardS/CardS";
+import { useState } from "react";
+import { Ionicons } from '@expo/vector-icons'; 
 
 const CercaTuyo = () => {
 
+    const [search, setSearch] = useState("")
     const locales = [
         {
             name: "Local uno"
@@ -22,21 +25,36 @@ const CercaTuyo = () => {
 
     return(
         <View style={{paddingBottom: 30}}>
-            <View style={styles.buscadorContainer}>
-                <AntDesign name="search1" size={24} color="#CCCCCC" />
-                <TextInput
-                style={styles.textInput}
+            <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+                <View style={styles.buscadorContainer}>
+                    <AntDesign name="search1" size={24} color="#A4A4A4" />
+                    <TextInput
+                    style={styles.textInput}
+                    onChangeText={setSearch}
+                    value={search}
+                    />
+                </View>
+                {search === "" ? "" : 
+                <TouchableOpacity style={{paddingEnd: 20}}>
+                    <Ionicons name="filter" size={30} color="#A4A4A4" />
+                </TouchableOpacity>
+                }
+            </View>
+            {search === "" ?
+            <View>
+                <CardXl/>
+                <Text style={{fontWeight: "bold", fontSize: 22, marginStart: 20, marginBottom: 20}}>20 minutos o menos</Text>
+                <FlatList
+                data={locales}
+                renderItem={({item}) => <CardS data={item}/>}
+                keyExtractor={item => item.name}
+                horizontal={true}
+                style={styles.listaContainer}
                 />
             </View>
-            <CardXl/>
-            <Text style={{fontWeight: "bold", fontSize: 22, marginStart: 20, marginBottom: 20}}>20 minutos o menos</Text>
-            <FlatList
-            data={locales}
-            renderItem={({item}) => <CardS data={item}/>}
-            keyExtractor={item => item.name}
-            horizontal={true}
-            style={styles.listaContainer}
-            />
+            :
+            ""
+            }
         </View>
     )
 }
@@ -50,6 +68,7 @@ const styles = StyleSheet.create({
         margin: 20,
         flexDirection: "row",
         alignItems: "center",
+        flex: 1
     },
     textInput:{
         marginStart: 20
